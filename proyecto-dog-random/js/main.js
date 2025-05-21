5449d9fb4d2ea60d75e0034221b5bcae49fd414f
Ana Rotela <anarotela18@gmail.com>
2025-05-21 17:41:54 -0300

    headers api key added

--- proyecto-dog-random/js/main.js
+++ proyecto-dog-random/js/main.js
@@ -1,13 +1,14 @@
-const API_URL_RANDOM =
-  "https://api.thedogapi.com/v1/images/search?limit=2&api_key=live_kQ8qbO0k5JISXH0gFhPDxyAanhQNOZ83UTVgYycOxf2WA0GDdWqunpc48zUsZ74d";
+const API_KEY =
+  "live_kQ8qbO0k5JISXH0gFhPDxyAanhQNOZ83UTVgYycOxf2WA0GDdWqunpc48zUsZ74d";
 
-const API_URL_FAVORITES =
-    "https://api.thedogapi.com/v1/favourites?&api_key=live_kQ8qbO0k5JISXH0gFhPDxyAanhQNOZ83UTVgYycOxf2WA0GDdWqunpc48zUsZ74d&order=DESC";
+const API_URL_RANDOM = "https://api.thedogapi.com/v1/images/search?limit=2";
+
+const API_URL_FAVORITES = "https://api.thedogapi.com/v1/favourites?&order=DESC";
 
 const API_URL_FAVORITES_DELETE = (id) =>
-  `https://api.thedogapi.com/v1/favourites/${id}?api_key=live_kQ8qbO0k5JISXH0gFhPDxyAanhQNOZ83UTVgYycOxf2WA0GDdWqunpc48zUsZ74d`;
+  `https://api.thedogapi.com/v1/favourites/${id}?`;
 
-function showMessage(type,text){
+function showMessage(type, text) {
   const messageDiv = document.getElementById("message");
   if (!messageDiv) {
     console.warn("⚠️ Element with ID 'message' not found.");
@@ -28,95 +29,100 @@ function showMessage(type,text){
 
   alert.appendChild(closeBtn);
   messageDiv.appendChild(alert);
-
-  setTimeout(() => {
-    const alertElement = messageDiv.querySelector(".alert");
-    if (alertElement) {
-      const alert = bootstrap.Alert.getOrCreateInstance(alertElement);
-      alert.close();
-    }
-  }, 4000);
 }
 
 async function loadRandomDogs() {
   try {
-    const response = await fetch(API_URL_RANDOM);
-    const data     = await response.json();
-    if (!response.ok){
-        showMessage("danger", `There was an error: ${response.status} ${data.message}`);
-    }else{
-        const dogImage1        = document.getElementById("dogImage1");
-        const dogImage2        = document.getElementById("dogImage2");
-
-        const dogButtonRandom1 = document.getElementById("dogButtonRandom1");
-        const dogButtonRandom2 = document.getElementById("dogButtonRandom2");
-
-        dogImage1.src = data[0].url;
-        dogImage2.src = data[1].url;
-
-        dogButtonRandom1.onclick = () => saveFavoriteDog(data[0].id);       
-        dogButtonRandom2.onclick = () => saveFavoriteDog(data[1].id);
+    const response = await fetch(API_URL_RANDOM, {
+      method: "GET",
+      headers: { "X-API-KEY": API_KEY },
+    });
+    if (!response.ok) {
+      const text = await response.text();
+      const errorMessage = `There was an error: ${response.status} ${text}`;
+      showMessage("danger", errorMessage);
+      return;
     }
-    
+    const data = await response.json();
+    if (data.length >= 2) {
+      const dogImage1 = document.getElementById("dogImage1");
+      const dogImage2 = document.getElementById("dogImage2");
+      
+      const dogBreed1 = document.getElementById("dogBreed1");
+      const dogBreed2 = document.getElementById("dogBreed2");
+
+      const dogButtonRandom1 = document.getElementById("dogButtonRandom1");
+      const dogButtonRandom2 = document.getElementById("dogButtonRandom2");
+
+      dogImage1.src = data[0].url;
+      dogImage2.src = data[1].url;
+
+      dogBreed1.textContent = data[0].breeds?.[0]?.name ?? "Unknown breed";
+      dogBreed2.textContent = data[1].breeds?.[0]?.name ?? "Unknown breed";
+
+      dogButtonRandom1.onclick = () => saveFavoriteDog(data[0].id);
+      dogButtonRandom2.onclick = () => saveFavoriteDog(data[1].id);
+    }
   } catch (error) {
     showMessage("danger", `Failed to fetch dog image: ${error}`);
   }
 }
 
-function createFavoriteDogCard(dog){
+function createFavoriteDogCard(dog) {
   const col = document.createElement("div");
   col.className = "col-12 col-sm-6 col-md-4 col-lg-3";
-  
+
   const article = document.createElement("article");
   article.className = "card h-100";
 
   const img = document.createElement("img");
   img.src = dog.image.url;
   img.alt = "Favorite dog";
-  img.className = "card-img-top"
+  img.className = "card-img-top mb-2";
 
   const cardBody = document.createElement("div");
   cardBody.className = "card-body text-center";
 
   const btn = document.createElement("button");
   btn.innerHTML = '<i class="bi bi-trash3"></i> Remove dog from favorites';
-  btn.className = "btn btn-outline-danger w-100 mt-2";
+  btn.className = "btn btn-outline-danger w-100";
   btn.onclick = () => deleteFavoriteDog(dog.id);
 
   cardBody.appendChild(btn);
   article.appendChild(img);
   article.appendChild(cardBody);
   col.appendChild(article);
-  
+
   return col;
 }
 async function loadFavoriteDogs() {
   try {
-    const response = await fetch(API_URL_FAVORITES);
-    //   "https://api.thecatapi.com/v1/favourites?limit=2",
-    //   {
-    //     headers: {
-    //       "content-type": "application/json",
-    //       "x-api-key":
-    //         "live_yIwxanfCOOGUH7jMAqvbpFdr9nqlpq6npXLEL642gyNZZcrjUZRJteVpAHqO7mKE",
-    //     },
-    //   }
-    // );
-    const data     = await response.json();
-    if (!response.ok) {
-      showMessage("danger", `There was an error in favorites: " ${response.status} - ${data.message}`);
-    }else{
-      const section = document.getElementById("favoritesContainer");
-      section.innerHTML = "";
+    const response = await fetch(API_URL_FAVORITES, {
+      method: "GET",
+      headers: { "X-API-KEY": API_KEY },
+    });
 
-      data.forEach(dog => { 
+    if (!response.ok) {
+      const text = await response.text();
+      const errorMessage = `There was an error in favorites: ${response.status} - ${text}`;
+      showMessage("danger", errorMessage);
+      return;
+    }
+
+    const data = await response.json();
+    const section = document.getElementById("favoritesContainer");
+    section.innerHTML = "";
+
+    if (Array.isArray(data)) {
+      data.forEach((dog) => {
         const card = createFavoriteDogCard(dog);
         section.appendChild(card);
       });
+    } else {
+      showMessage("danger", "Unexpected response format from favorites API.");
     }
   } catch (error) {
-    showMessage("danger", "An unexpected error ocurred on loadFavoriteDogs");
-
+    showMessage("danger", `An unexpected error ocurred on loadFavoriteDogs ${error}`);
   }
 }
 
@@ -126,19 +132,22 @@ async function saveFavoriteDog(id) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "X-API-KEY": API_KEY,
       },
       body: JSON.stringify({
         image_id: id,
       }),
     });
-    const data = await response.json();
 
     if (!response.ok) {
-      showMessage("danger",`There was an error in save: " ${response.status}  - ${data.message}`);
-    } else {
-      showMessage("success", "Dog saved into favorites");
-      loadFavoriteDogs();
+      const text = await response.text();
+      let errorMessage = `There was an error in save: ${response.status} - ${text}`;
+      showMessage("danger", errorMessage);
+      return;
     }
+
+    showMessage("success", "Dog added to favorites");
+    loadFavoriteDogs();
   } catch (error) {
     showMessage("danger", "An unexpected error ocurred on saveFavoriteDog");
   }
@@ -148,15 +157,17 @@ async function deleteFavoriteDog(id) {
   try {
     const response = await fetch(API_URL_FAVORITES_DELETE(id), {
       method: "DELETE",
+      headers: { "X-API-KEY": API_KEY },
     });
-    const data = await response.json();
 
     if (!response.ok) {
-      showMessage("danger",`There was an error in delete: " ${response.status}  ${data.message}`);
-    } else {
-      showMessage("success", "Dog deleted into favorites");
-      loadFavoriteDogs();
+      const text = await response.text();
+      const errorMessage = `There was an error in delete: ${response.status} - ${text}`;
+      showMessage("danger", errorMessage);
+      return;
     }
+    showMessage("success", "Dog removed from favorites");
+    loadFavoriteDogs();
   } catch (error) {
     showMessage("danger", "An unexpected error ocurred on deleteFavoriteDog");
   }
